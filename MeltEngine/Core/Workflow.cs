c60442f36da903a42c9c3b638672bd0cf28d33ce
David Pino <aesthezel@gmail.com>
2022-02-11 09:02:04 -0400

    Passed test

--- MeltEngine/Core/Workflow.cs
+++ MeltEngine/Core/Workflow.cs
@@ -23,8 +23,7 @@ namespace MeltEngine.Core
             {
                 // Update
                 OnUpdate?.Invoke();
-                goTest.Enabled = !goTest.Enabled;
-                
+
                 // Draw frame
                 Raylib.BeginDrawing();
                 Raylib.ClearBackground(Raylib.RAYWHITE); // TODO: Add into a scene...
